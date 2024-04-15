e224dd146fd18ee8310297c5964a6a4755f51dd1
Damian Chojnacki <daamian3@gmail.com>
2024-04-15 22:42:46 +0200

    feature: add entrypoint [skip ci]

---
+++ docker/prod/entrypoint.sh
@@ -0,0 +1,13 @@
+touch /var/www/html/storage/database.sqlite
+
+php /var/www/html/artisan migrate --force
+
+php /var/www/html/artisan config:cache --no-ansi -q
+php /var/www/html/artisan route:cache --no-ansi -q
+php /var/www/html/artisan view:cache --no-ansi -q
+php /var/www/html/artisan event:cache --no-ansi -q
+
+if [ $# -gt 0 ]; then
+    # If we passed a command, run it as root
+    exec "$@"
+fi
