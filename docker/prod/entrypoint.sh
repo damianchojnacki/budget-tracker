d6aef6392e51eeda271b0c21fbd3d0ed0e0d95c7
Damian Chojnacki <daamian3@gmail.com>
2024-04-16 09:33:24 +0200

    feature: add nginx [skip ci]

--- docker/prod/entrypoint.sh
+++ docker/prod/entrypoint.sh
@@ -4,9 +4,13 @@ touch /var/www/html/storage/database.sqlite
 
 php /var/www/html/artisan migrate --force
 
+mkdir -p /var/www/html/storage/logs /var/www/html/storage/framework/cache /var/www/html/storage/framework/views /var/www/html/storage/framework/sessions
+
 php /var/www/html/artisan config:cache --no-ansi -q
 php /var/www/html/artisan route:cache --no-ansi -q
 php /var/www/html/artisan view:cache --no-ansi -q
 php /var/www/html/artisan event:cache --no-ansi -q
+
+sudo cp -r /var/www/html/* /code
 
 exec "$@"
